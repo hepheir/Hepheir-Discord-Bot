dd1e4041ce2ac2dac3b28ea8a2885ba404faa55a
Hepheir <hepheir@gmail.com>
2018-07-23 22:12:18 +0900

    separate msg listener and commands

--- index.js
+++ index.js
@@ -20,37 +20,57 @@ var STRING = {
     hereIAm : "왔다 왔다, 내가 왔다!"
 };
 
+var COMMAND = [
+    {
+        names : ['노래'],
+        action : msg => {
+            if (!msg.member.voiceChannel) {
+                msg.reply(`음성 채널에 먼저 접속한 뒤에 불러주세요.`);
+            }
+    
+            BOT.voiceChannel = msg.member.voiceChannel;
+            BOT.voiceChannel.join()
+                .then(connection => {
+                    BOT.dispatcher = connection.playFile('./test.mp3');
+                    BOT.dispatcher.on('end', () => { BOT.dispatcher = undefined });
+                })
+                .catch(e => {
+                    console.log(e);
+                    BOT.dispatcher = undefined;
+                })
+        }
+    },
+    {
+        names : ['나가', '꺼져', '싫어'],
+        action : msg => {
+            if (!BOT.voiceChannel) return;
+
+            if (BOT.dispatcher) {
+                BOT.dispatcher.end();
+            }
+
+            BOT.voiceChannel.leave();
+            BOT.voiceChannel = undefined;
+        }
+    },
+    {
+        names : ['볼륨'],
+        action : msg => {}
+    },
+    {
+        names : [],
+        action : msg => {}
+    }
+];
 
 
 client.on('message', msg => {
     if (!msg.guild || msg.author.bot) return;
 
-    if (msg.content === '나가' || msg.content === '꺼져' || msg.content === '싫어') {
-        if (!BOT.voiceChannel) return;
-
-        if (BOT.dispatcher) {
-            BOT.dispatcher.end();
+    COMMAND.forEach(c => c.names.forEach( cmd => {
+        if (msg.content.startsWith(cmd)) {
+            c.action(msg);
+            return;
         }
-
-        BOT.voiceChannel.leave();
-        BOT.voiceChannel = undefined;
-    }
-
-    if (msg.content === '노래') {
-        
-        if (!msg.member.voiceChannel) {
-            msg.reply(`음성 채널에 먼저 접속한 뒤에 불러주세요.`);
-        }
-
-        BOT.voiceChannel = msg.member.voiceChannel;
-        BOT.voiceChannel.join()
-            .then(connection => {
-                BOT.dispatcher = connection.playFile('./test.mp3');
-                BOT.dispatcher.on('end', () => { BOT.dispatcher = undefined });
-            })
-            .catch(e => {
-                console.log(e);
-                BOT.dispatcher = undefined;
-            })
-    }
+    }));
 });
