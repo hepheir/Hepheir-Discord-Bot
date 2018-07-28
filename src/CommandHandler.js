9ebec638830b435db6669fcdadb1b9c049cb9e0c
Hepheir <hepheir@gmail.com>
2018-07-29 01:23:21 +0900

    Deprecated

--- src/CommandHandler.js
+++ src/CommandHandler.js
@@ -1,102 +1,52 @@
 const Command = require('./Command.js');
 
+
 class CommandHandler {
-    constructor(CommandHandler) {
-        // @public
-        this.Client = CommandHandler.Client;
+
+constructor(Parent_CommandHandler) {
+    // @static
     
-        // @static
-        this.action = {
-            list     : new Array(),
-            register : this.register
+    // @public
+    this.history = {
+        list    : new Array(),
+        add     : this._addHistory,
+        clear   : this._clearHistory
+    };
+
+    if (this.PARENT_COMMAND_HANDLER.history != undefined) {
+        if (Array.isArray(this.PARENT_COMMAND_HANDLER.history.list)) {
+            this.history.list = this.PARENT_COMMAND_HANDLER.history.list;
         }
-        
-        // @public
-        this.history = {
-            list    : new Array(),
-            add     : this._addHistory,
-            clear   : this._clearHistory
-        };
-
-        if (CommandHandler.history != undefined) {
-            if (Array.isArray(CommandHandler.history.list)) {
-                this.history.list = CommandHandler.history.list;
-            }
-        }
-
-        // Bindings
-            this.onMessage = this.onMessage.bind(this);
-            this.register  = this.register.bind(this);
-            this.getAction = this.getAction.bind(this);
-
-            this._addHistory   = this._addHistory.bind(this);
-            this._clearHistory = this._clearHistory.bind(this);
     }
 
-    // Message Listener
-        onMessage(MessageObject) {
-            this.history.add(MessageObject);
+    // Bindings
+    this.onMessage = this.onMessage.bind(this);
 
-            let Action = this.action.list.find(act => act.condition.check(MessageObject));
-            
-            if (Action != undefined) {
-                this.Client.user.setStatus('dnd');
-                let nextAction = Action.call(this);
-                
-                if (nextAction) {
-                    this.Client.user.setStatus('dnd');
-                    nextAction(this);
-                }
-            }
-            this.Client.user.setStatus('online');
-        }
+    this._addHistory   = this._addHistory.bind(this);
+    this._clearHistory = this._clearHistory.bind(this);
+}
 
-    // Command
-        register(name, option, onCommand = true, onChat = false, onMention = false, onBot = false) {
-            if (!name) {
-                throw 'Command.name must be given.';
-            }
-
-            let Cmd = new Command(name, option.action);
-
-            for (let opt in option) {
-                if (opt == 'subnames') {
-                    Cmd.subnames = Array.isArray(option.subnames) ?
-                        option.subnames :
-                        new Array();
-                }
-                else if (opt == 'action') {
-                    Cmd.action = typeof option.action === 'function' ?
-                        option.action :
-                        function() {};
-                }
-                Cmd[opt] = option[opt];
-            }
-
-            Cmd.condition.onCommand = onCommand;
-            Cmd.condition.onChat    = onChat;
-            Cmd.condition.onMention = onMention;
-            Cmd.condition.onBot     = onBot;
-
-            this.action.list.push(Cmd);
-            return Cmd;
-        }
-
-        getAction(name) {
-            return this.action.list.find(act => act.name === name);
-        }
-
-
-    // History Handling
-        _addHistory(MessageObject) {    
-            return this.list.unshift(MessageObject);
-        }
-        
-        _clearHistory() {
-            this.list = new Array();
-        }
+get client() {
+    return this.PARENT_COMMAND_HANDLER.client;
 }
 
 
+// Message Listener
+onMessage(Message, Listener) {
+
+    this.history.add(Message);
+    
+    let target = this.COMMAND_LIST.find(act => act.isThis(Message));
+    if (target === undefined) return;
+
+    let next = target.call(this);
+    if (next) next(this);
+
+    this.client.once('message', Listener);
+}
+
+
+
+} // Class End
 
 module.exports = CommandHandler
