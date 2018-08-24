23911729a8c8b73b83e4f713932818168396d909
김동주 <hepheir@gmail.com>
2018-08-25 04:34:29 +0900

    Conflate login.js to index.js

---
+++ bot/index.js
@@ -0,0 +1,9 @@
+// Xiaho Zin DDang Bot - launch and login
+// by Hepheir, 2018
+// remembering KimSuBin
+
+
+const Client = require('./app/login.js');
+
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
