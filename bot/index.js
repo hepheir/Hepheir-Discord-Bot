f92e159a8c8f531d2e5fef7daf8f8ea557654964
김동주 <hepheir@gmail.com>
2018-08-28 08:31:39 +0900

    Move login functions to this file.

--- bot/index.js
+++ bot/index.js
@@ -1,9 +1,71 @@
-// Xiaho Zin DDang Bot - launch and login
+// Xiaho Zin DDang Bot - Launch and Login
 // by Hepheir, 2018
 // remembering KimSuBin
 
+'use strict';
 
-const Client = require('./app/login.js');
+///////////////////////////////////////
+
+const API_Bot_Path = '../common/api/bot.json';
+
+///////////////////////////////////////
+
+const fs = require('fs');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 const events = require('events');
 const eventEmitter = new events.EventEmitter();
+
+///////////////////////////////////////
+
+var Brains = {
+    //
+};
+
+///////////////////////////////////////
+
+function __main__() {
+    clientLogin();
+
+    client.once('ready', () => {
+        //
+    });
+}
+
+///////////////////////////////////////
+
+function clientLogin() {
+    let BOT = readJsonFile(API_Bot_Path);
+    if (!BOT.token) throw `
+Error! Could not login because the token was not given!
+please enter your token @ [bot/common/api/bot.json]`;
+    
+    
+    client.login(BOT.token);
+    client.on('ready', () => { console.log(`
+('-')/ Yo!
+Successfully logged in as [${client.user.tag}]!
+
+You can invite [${client.user.tag}] on this url.
+[https://discordapp.com/oauth2/authorize?client_id=${BOT.clientId}&scope=bot&permissions=108133440]`);
+
+    });
+}
+
+
+function __commandRegistry__() {
+
+}
+
+///////////////////////////////////////
+
+function readJsonFile(path) {
+    let file = fs.readFileSync(path);
+    return JSON.parse(file);
+}
+
+///////////////////////////////////////
+
+__main__();
