fa1638f3ba859bbea1c0d79a5e08066549f38456
Hepheir <hepheir@gmail.com>
2021-02-07 03:03:53 +0900

    Add command `nothing`

---
+++ commands/nothing.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'nothing',
+    description: 'Literally does nothing!',
+    aliases: ['hepheir', 'hep'],
+    args: false,
+    usage: 'nothing',
+    cooldown: 2,
+    guildOnly: false,
+    reaction: "<:hepheir:806550414162001970>",
+    execute(message, args) {
+        return;
+    },
+};
