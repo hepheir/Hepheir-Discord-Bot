0ce7632e1c4179285b692855fee557581cd1b9e4
Hepheir <hepheir@gmail.com>
2021-02-07 03:04:07 +0900

    Add command `rghb`

---
+++ commands/rghb.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'rghb',
+    description: '라고할뻔 ㅋㅋ!',
+    args: false,
+    usage: 'nothing',
+    cooldown: 10,
+    guildOnly: false,
+    reaction: "<a:ko_RGHB:806550357731442731>",
+    execute(message, args) {
+        return;
+    },
+};
